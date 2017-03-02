4974d4bf48def01ef46e1108d009bb82ef9f26d4
hsimyu <samisigena.sazanami@gmail.com>
2017-03-03 00:56:12 +0900

    ほしいものリストのURLを更新.

--- src/advertisement.js
+++ src/advertisement.js
@@ -5,7 +5,7 @@ var Advertisement = React.createClass({
     render: function() {
         return (
             <div style={{width: "100%", overflow: "hidden"}}>
-              <a href="http://www.amazon.co.jp">{intl.translate("応援テキスト", this.props.locale)}</a>
+              <a target="_blank" href="http://www.amazon.co.jp/registry/wishlist/1BWHTN1R6SMDI/ref=cm_sw_r_tw_ws_x_85dUybS1Q824K">{intl.translate("応援テキスト", this.props.locale)}</a>
             </div>
         );
     },
