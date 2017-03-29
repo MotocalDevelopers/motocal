edab6246f9c638defe2245a7e8afe7b12c009a86
hsimyu <samisigena.sazanami@gmail.com>
2017-03-29 15:36:32 +0900

    スマホ版広告の位置を変更.

--- webpack.config.js
+++ webpack.config.js
@@ -38,19 +38,19 @@ const plugins = [
       GITHUB_API_TOKEN: JSON.stringify(process.env.GITHUB_API_TOKEN || ''),
     },
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      screw_ie8: true, // React doesn't support IE8
-      warnings: false,
-    },
-    mangle: {
-      screw_ie8: true,
-    },
-    output: {
-      comments: false,
-      screw_ie8: true,
-    },
-  }),
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     screw_ie8: true, // React doesn't support IE8
+  //     warnings: false,
+  //   },
+  //   mangle: {
+  //     screw_ie8: true,
+  //   },
+  //   output: {
+  //     comments: false,
+  //     screw_ie8: true,
+  //   },
+  // }),
   new ManifestPlugin({
     fileName: 'asset-manifest.json',
   }),
