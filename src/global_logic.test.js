07147b0a69f5db608f7d43fbbcadee91053c5d2f
kei <45387555+kei-gbf@users.noreply.github.com>
2019-04-11 18:27:46 +0900

    Add negative defense debuff number tests
      * When defense debuff was negative number, it's defense up.
      * Fix defense number notation in the translation.
        no need floating point number for now.

--- src/global_logic.test.js
+++ src/global_logic.test.js
@@ -8,7 +8,25 @@ describe('#getTypeBonus', () => {
 
 
 describe('#calcDefenseDebuff', () => {
-  test('when defense debuff is over 100, enemy defense is 1', () => {
+  test('when enemyDefense and defenseDebuff is not set, defense is 10', () => {
+    expect(calcDefenseDebuff(undefined, undefined)).toBe(10)
+  })
+
+  test('when enemyDefense is not set, the default value is 10', () => {
+    expect(calcDefenseDebuff(undefined, 0)).toBe(10)
+    expect(calcDefenseDebuff(undefined, 50)).toBe(5)
+  })
+
+  test('when defenseDebuff is not set, the default value is 0', () => {
+    expect(calcDefenseDebuff(10, undefined)).toBe(10)
+    expect(calcDefenseDebuff(20, undefined)).toBe(20)
+  })
+
+  test('when defenseDebuff was negative numbers, defense is up', () => {
+  	expect(calcDefenseDebuff(10, -50)).toBe(15)
+  })
+
+  test('when defense debuff is over 100, defense is 1', () => {
       expect(calcDefenseDebuff(10, 100)).toBe(1)
   })
 })
