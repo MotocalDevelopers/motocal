68ff18d5edddb6650160324a489f64f6319d4837
Jongaros <orkunkocyigit@gmx.com>
2019-05-07 21:08:22 +0300

    Migrated from extract-text-webpack-plugin to mini-css-extract-plugin as Webpack 4 doesn't support it

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -11,100 +11,103 @@ const DEBUG = process.env.NODE_ENV === 'development' || process.env.NODE_ENV ===
 const devtool = DEBUG ? '#inline-source-map' : '#eval';
 const fileName = DEBUG ? '[name]' : '[name]-[hash]';
 const plugins = [
-  new ExtractTextPlugin({
-    filename: `${fileName}.css`,
-    disable: false,
-    allChunks: true
-  }),
-  new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'public/index.html'),
-    inject: false,
-    favicon: path.join(__dirname, 'public/favicon.ico'),
-    files:{
-      "js": `${fileName}.js`,
-      "css": `${fileName}.css`
-    },
-    minify: {
-      removeComments: true,
-      // collapseWhitespace: true,
-      removeRedundantAttributes: true,
-      useShortDoctype: true,
-      removeEmptyAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      keepClosingSlash: true,
-      // minifyJS: true,
-      // minifyCSS: true,
-      // minifyURLs: true,
-    },
-    ADSENSE_AD_CLIENT: process.env.ADSENSE_AD_CLIENT,
-    ADSENSE_AD_SLOT_PC1: process.env.ADSENSE_AD_SLOT_PC1,
-    ADSENSE_AD_SLOT_PC2: process.env.ADSENSE_AD_SLOT_PC2,
-    ADSENSE_AD_SLOT_MOBILE: process.env.ADSENSE_AD_SLOT_MOBILE,
-    GOOGLE_ANALYTICS_TRACKING_ID: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-  }),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-      GITHUB_API_TOKEN: JSON.stringify(process.env.GITHUB_API_TOKEN || ''),
-      TWITTER_ID: JSON.stringify(process.env.TWITTER_ID || ''),
-    },
-  }),
-  CopyWebpackPlugin([
-    { from: path.join(__dirname, "/*Data.json"), to: path.join(__dirname, "/dist/") },
-    { from: path.join(__dirname, "/imgs/"), to: path.join(__dirname, "/dist/imgs/"), ignore: [ '*.txt' ]},
-    { from: path.join(__dirname, "/otherImages/"), to: path.join(__dirname, "/dist/otherImages/") },
-    { from: path.join(__dirname, "/charaimgs/"), to: path.join(__dirname, "/dist/charaimgs/"), ignore: [ '*.txt' ]},
-  ]),
-  // new webpack.optimize.UglifyJsPlugin({
-  //   compress: {
-  //     screw_ie8: true, // React doesn't support IE8
-  //     warnings: false,
-  //   },
-  //   mangle: {
-  //     screw_ie8: true,
-  //   },
-  //   output: {
-  //     comments: false,
-  //     screw_ie8: true,
-  //   },
-  // }),
-  new ManifestPlugin({
-    fileName: 'asset-manifest.json',
-  }),
+    new MiniCssExtractPlugin({
+        filename: `${fileName}.css`,
+    }),
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public/index.html'),
+        inject: false,
+        favicon: path.join(__dirname, 'public/favicon.ico'),
+        files: {
+            "js": `${fileName}.js`,
+            "css": `${fileName}.css`
+        },
+        minify: {
+            removeComments: true,
+            // collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            // minifyJS: true,
+            // minifyCSS: true,
+            // minifyURLs: true,
+        },
+        ADSENSE_AD_CLIENT: process.env.ADSENSE_AD_CLIENT,
+        ADSENSE_AD_SLOT_PC1: process.env.ADSENSE_AD_SLOT_PC1,
+        ADSENSE_AD_SLOT_PC2: process.env.ADSENSE_AD_SLOT_PC2,
+        ADSENSE_AD_SLOT_MOBILE: process.env.ADSENSE_AD_SLOT_MOBILE,
+        GOOGLE_ANALYTICS_TRACKING_ID: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+            GITHUB_API_TOKEN: JSON.stringify(process.env.GITHUB_API_TOKEN || ''),
+            TWITTER_ID: JSON.stringify(process.env.TWITTER_ID || ''),
+        },
+    }),
+    CopyWebpackPlugin([
+        {from: path.join(__dirname, "/*Data.json"), to: path.join(__dirname, "/dist/")},
+        {from: path.join(__dirname, "/imgs/"), to: path.join(__dirname, "/dist/imgs/"), ignore: ['*.txt']},
+        {from: path.join(__dirname, "/otherImages/"), to: path.join(__dirname, "/dist/otherImages/")},
+        {from: path.join(__dirname, "/charaimgs/"), to: path.join(__dirname, "/dist/charaimgs/"), ignore: ['*.txt']},
+    ]),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     screw_ie8: true, // React doesn't support IE8
+    //     warnings: false,
+    //   },
+    //   mangle: {
+    //     screw_ie8: true,
+    //   },
+    //   output: {
+    //     comments: false,
+    //     screw_ie8: true,
+    //   },
+    // }),
+    new ManifestPlugin({
+        fileName: 'asset-manifest.json',
+    }),
 ];
 
 
 module.exports = {
-  devtool: devtool,
-  entry: ['./src/content.js', './css/style.css', './css/smartphone.css'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: `${fileName}.js`,
-  },
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader",
-            publicPath: "css",
-           })
-      },
-  ]},
-  plugins: plugins,
-  devServer: {
-     contentBase: [path.join(__dirname, "/dist")],
-     compress: true,
-     host: "localhost",
-     port: 8000
-  },
-  mode: process.env.NODE_ENV || 'development'
+    devtool: devtool,
+    entry: ['./src/content.js', './css/style.css', './css/smartphone.css'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: `${fileName}.js`,
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: 'css'
+                        }
+                    },
+                    'css-loader'
+                ]
+            },
+        ]
+    },
+    plugins: plugins,
+    devServer: {
+        contentBase: [path.join(__dirname, "/dist")],
+        compress: true,
+        host: "localhost",
+        port: 8000
+    },
+    mode: process.env.NODE_ENV || 'development'
 };
