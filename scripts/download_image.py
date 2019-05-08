6a588869b3fcb48cddc77b5db7efb5329e579d98
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2019-05-08 15:58:10 +0300

    Added multiline reporting because I like watching pretty bars. Added overwrite option

--- scripts/download_image.py
+++ scripts/download_image.py
@@ -40,16 +40,17 @@ SAVE_DIR = {
 }
 
 
-def progress_reporter(count, total, path=''):
+def progress_reporter(count, total, path='', multiline=True):
     bar_len = 45
     filled_len = int(round(bar_len * count / float(total)))
-    status = basename(path)
 
+    status = basename(path)
     percents = round(100.0 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-    stdout.write('[%s] %s%s ...%20s\r' % (bar, percents, '%', status))
-    stdout.flush()
+    if not multiline:
+        print('[%s] %s%s ...%20s' % (bar, percents, '%', status), flush=True, end='\r')
+    else:
+        print('[%s] %s%s ...%20s' % (bar, percents, '%', status), flush=True)
 
 
 def plain_reporter(count, total, url=''):
@@ -74,6 +75,7 @@ def main(argvs):
       * --dry-run -d: Prints url list without download.
       * --workers -w: Number of threads to download images.
       * --reporter -r: (progress|plain)
+      * --overwrite -o: Redownloads all images even if it exists
     """
     parser = create_parser()
     if len(argvs) == 1:  # if only 1 argument, it's the script name
@@ -113,7 +115,7 @@ def main(argvs):
             _path = abspath(join(options.save_dir, name))
             yield _url, _path
 
-    def scan_download(_stream, override=False):
+    def scan_download(_stream):
         """
         Filtering non-exists files
 
@@ -121,10 +123,10 @@ def main(argvs):
         pass this param if implement force download option in future.
         """
         for _url, _path in parse_file(_stream):
-            if override or not exists(_path):
+            if options.overwrite or not exists(_path):
                 yield _url, _path
 
-    def download_image(durl, dpath, num):
+    def download_image(durl, dpath):
         if not options.dry_run:
             urlretrieve(durl, dpath)
 
@@ -134,11 +136,9 @@ def main(argvs):
         total = len(items)
         if total > 0:
             with ThreadPoolExecutor(max_workers=options.workers) as executor:
-                future_to_image = {executor.submit(download_image, url, path, num): (url, path, num) for
-                                   num, (url, path) in
-                                   enumerate(items, start=1)}
-                for future in as_completed(future_to_image):
-                    url, path, num = future_to_image[future]
+                future_to_image = {executor.submit(download_image, url, path): (url, path) for (url, path) in items}
+                for num, future in enumerate(as_completed(future_to_image), start=1):
+                    url, path = future_to_image[future]
                     report(num, total, path if options.reporter == 'progress' else url)
 
 
@@ -147,10 +147,11 @@ def create_parser():
     parser.add_option('--target', '-t', action='store', dest="target", default="arm", choices=list(SAVE_DIR.keys()))
     parser.add_option('--site', action='store', dest="site", default="wiki", choices=["wiki", "game"])
     parser.add_option('--save_dir', action='store', dest="save_dir", default=None)
-    parser.add_option('--dry-run', '-d', action='store_true', dest="dry_run", )
+    parser.add_option('--dry-run', '-d', action='store_true', dest="dry_run")
     parser.add_option('--workers', '-w', action='store', dest="workers", type='int', default=1)
     parser.add_option('--reporter', '-r', action='store', dest="reporter", default='progress',
                       choices=['progress', 'plain'])
+    parser.add_option('--overwrite', '-o', action='store_true', dest="overwrite")
     return parser
 
 
