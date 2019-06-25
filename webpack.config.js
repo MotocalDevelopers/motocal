796f8206a600789a760cefb9c3409a190f805f57
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2019-06-25 12:44:53 +0300

    Added polyfill for safari support.

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ const plugins = [
 
 module.exports = {
   devtool: devtool,
-  entry: ['./src/content.js', './css/style.css', './css/smartphone.css'],
+  entry: ['./node_modules/datalist-polyfill/datalist-polyfill.js', './src/content.js', './css/style.css', './css/smartphone.css'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: `${fileName}.js`,
