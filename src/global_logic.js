a2c5c6bcd6c5214f7ba94bbb29602a58f6e75699
h-yasha <husssaindesu@gmail.com>
2019-07-09 11:28:59 +0300

    Limit expectedOugiGage to min 0

--- src/global_logic.js
+++ src/global_logic.js
@@ -676,7 +676,8 @@ module.exports.calcBasedOneSummon = function (summonind, prof, buff, totals) {
         var ougiGageBuff = buff["ougiGage"] + totals[key]["ougiGageBuff"] + (0.01 * totals[key]["LB"]["OugiGageBuff"]) - totals[key]["ougiDebuff"];
         var expectedOugiGage = ougiGageBuff;
         expectedOugiGage *= uplift + (taRate * 37.0 + (1.0 - taRate) * (daRate * 22.0 + (1.0 - daRate) * 10.0));
-        
+        expectedOugiGage = Math.max(0, expectedOugiGage);
+
         var ougiGageUpOugiBuff = buff["ougiGageUpOugi"] * ougiGageBuff;
         var ougiGage = 100 - Math.min(99, ougiGageUpOugiBuff);
         var minimumTurn = Math.ceil(ougiGage / ((uplift + 37.0) * ougiGageBuff));
