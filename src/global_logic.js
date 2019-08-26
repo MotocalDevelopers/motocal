68e101a8a060c724b892a33b6a9b442ec0a38432
kei <45387555+kei-gbf@users.noreply.github.com>
2019-08-26 20:17:15 +0900

    Fix resonance staff
    - fixed it should stack not override
    - removed redundant limit value check
      damage calc has the limit check, no need on skill count time.
    - add limit check for countWand

--- src/global_logic.js
+++ src/global_logic.js
@@ -2100,7 +2100,7 @@ module.exports.addSkilldataToTotals = function (totals, comb, arml, buff) {
             chara["ex"] += Math.min(LIMIT.grandEpic, numGrandEpic * countEpic * 4.0);
         }
         if (numResonanceStaff > 0) {
-            chara["normalOtherSante"] = Math.min(50.0, countWand * 1.0);
+            chara["normalOtherSante"] += Math.min(10, countWand) * 1.0;
         }
         if (isAllUniqueArm) {
             chara["ex"] += 20.0;
