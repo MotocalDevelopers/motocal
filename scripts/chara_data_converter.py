03a50fccf89f3e319d96d7d972db35244c539d43
kei <45387555+kei-gbf@users.noreply.github.com>
2019-10-10 16:12:31 +0900

    Patch 2019-09-30: New characters, weapons, uncaps
    - New characters
      - Siegfried (Fire)
      - Monika (Grand)
      - La Coiffe (SR)
    - New weapons
      - Black Dragon's Maw
      - Sky Ace
    - Uncap characters
      - Rackam (Grand)
      - Katalina (Grand)

--- scripts/chara_data_converter.py
+++ scripts/chara_data_converter.py
@@ -508,6 +508,8 @@ patchingOugiRatio["サンダルフォン"] = {"ougiRatio": 5.0}
 patchingOugiRatio["ヴァンピィ"] = {"ougiRatio": 5.0}
 patchingOugiRatio["リリィ"] = {"ougiRatio": 5.0}
 patchingOugiRatio["アイル"] = {"ougiRatio": 5.0}
+patchingOugiRatio["カタリナ(リミテッドver)"] = {"ougiRatio": 5.0}
+patchingOugiRatio["ラカム(リミテッドver)"] = {"ougiRatio": 5.0}
 
 ### SSR (Guardian Deity 5★) (最終十二神将)
 patchingOugiRatio["アンチラ"] = {"ougiRatio": 5.5}
