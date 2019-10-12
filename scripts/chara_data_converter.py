99f19a01424d9b7e20f9f15f8280e174173327d8
kei <45387555+kei-gbf@users.noreply.github.com>
2019-10-12 18:30:23 +0900

    Fix converter script and uncapped chara image
    - Update fav br_pattern to support "fav1/fav2" format
    - Fix uncapped chara image (remove _full suffix)

--- scripts/chara_data_converter.py
+++ scripts/chara_data_converter.py
@@ -666,7 +666,7 @@ def get_value(value_str):
 
 def processCSVdata(csv_file_name, json_data, image_wiki_url_list, image_game_url_list):
     key_pattern = re.compile("(\d+_\d+\.png)")
-    br_pattern = re.compile("(\w+)&br;(\w+)")
+    br_pattern = re.compile("(\w+)(?:&br;|\/)(\w+)")
     support_pattern2 = re.compile("([\W\w]+)&br;([\W\w]+)")
     support_pattern3 = re.compile("([\W\w]+)&br;([\W\w]+)&br;([\W\w]+)")
     name_pattern = re.compile("\[\[([\W\w]+?) \((S?S?R)\)")
