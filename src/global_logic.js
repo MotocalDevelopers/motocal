b173f4d48510a032b09783336f06646a4adad31a
Groovman <37013381+Groovman@users.noreply.github.com>
2019-12-16 23:26:24 +0100

    fix me being tired

--- src/global_logic.js
+++ src/global_logic.js
@@ -2796,11 +2796,13 @@ module.exports.treatSupportAbility = function (totals, chara, comb, arml, buff) 
                 case "hanged_man_reversed":
                     if (index > 4) {
                         if (epic.isAllUniqueArm(arml, comb)) {
-                            if (totals[key].isConsideredInAverage && totals[key2]["element"] === support.range) {
+                            if (totals[key].isConsideredInAverage) {
                                 for (var key2 in totals) {
-                                    totals[key2]["caimOther"] += 0.20;
-                                    totals[key2]["normalDamageLimit"] += 0.10;
-                                    totals[key2]["ougiDamageLimit"] += 0.10;
+                                    if (totals[key2]["element"] === support.range) {
+                                        totals[key2]["caimOther"] += 0.20;
+                                        totals[key2]["normalDamageLimit"] += 0.10;
+                                        totals[key2]["ougiDamageLimit"] += 0.10;
+                                    }
                                 }
                             } else {
                                 // Calculate yourself only if you do not put it in the average
