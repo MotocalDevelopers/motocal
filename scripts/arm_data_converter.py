a8e0b7b88850152f0ae1fa42824505a1a6d32efd
kei <45387555+kei-gbf@users.noreply.github.com>
2020-03-07 20:55:47 +0900

    Update patch 2020-Feb
    - new chara
      - Lecia (Water)
      - Vania and Malinda
      - Rei (Grand)
      - Scathacha (Valentine)
      - Grimnir (Valentine)
    
    - new weapon
      - Commander's Sidearm
      - Plushie Pal
      - Sunya
      - Deirdre's Heart
      - Lupercalia
      - Honoikazuchi no Okami
    
    - uncap weapon
      - Gottfried
      - Ancient Bandages
      - Full Metal Recoil
      - Bloody Scar
      - Gangsta Knife
      - Another Sky
    
    This PR does not include uncap chara update.
    4stars Gangsta Knife required to merge previous PR,
    This PR may have conflicts.

--- scripts/arm_data_converter.py
+++ scripts/arm_data_converter.py
@@ -257,8 +257,12 @@ skillnamelist["normalKatsumiM"] = {
 }
 
 skillnamelist["normalElementM"] = {
+    u"業火の進境": "fire",
     u"渦潮の進境": "water",
-    u"雷電の進境": "light"
+    u"大地の進境": "earth",
+    u"竜巻の進境": "wind",
+    u"雷電の進境": "light",
+    u"奈落の進境": "dark"
 }
 
 skillnamelist["kaijinnoyogen"] = {
@@ -843,6 +847,7 @@ skillnamelist["strengthL"] = {
     u"残る霞火、あへなし": "light",
     u"暴風の残滓": "wind",
     u"ガチャピンの攻刃": "wind",
+    u"六波羅蜜の攻刃": "dark",
 }
 
 skillnamelist["exATKandHPM"] = {
@@ -1246,7 +1251,7 @@ def processCSVdata(csv_file_name, json_data, image_wiki_url_list, image_game_url
         if name in translation:
             newdict["en"] = translation[name]
         else:
-            print(name)
+            print("missing translate", name)
             newdict["en"] = name
 
         json_data[name] = newdict
